52fe15e2c24d947493807868a7dc59d0cd0c76c8
Tarbolin69 <tarbolin69@gmail.com>
2024-06-03 20:04:27 -0300

    algunos todo e ideas

--- src/productos/Producto.java
+++ src/productos/Producto.java
@@ -3,6 +3,8 @@ package productos;
 import java.time.LocalDate;
 import java.util.UUID;
 
+// Quiza armar una clase llamada "Stock" que sea solo una lista de Producto, para poder hacer
+// operaciones sobre todos los productos, como e print tabla
 public abstract class Producto {
     protected UUID numeroUnico = UUID.randomUUID();
     protected int precioBase;
