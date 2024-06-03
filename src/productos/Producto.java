8239ea58e5d7a4942f4cdcdc962d7c504a0b58d3
Tarbolin69 <tarbolin69@gmail.com>
2024-06-03 18:31:54 -0300

    clase de productos

---
+++ src/productos/Producto.java
@@ -0,0 +1,12 @@
+package productos;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public abstract class Producto {
+    protected UUID numeroUnico = UUID.randomUUID();
+    protected String nombreProducto;
+    protected int precioBase;
+    protected LocalDate fechaEnvase = LocalDate.now();
+    protected LocalDate fechaVencimiento;
+}
