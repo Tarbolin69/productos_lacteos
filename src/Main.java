549376f4552c197422aac399f16facc6d58efae5
Tarbolin69 <tarbolin69@gmail.com>
2024-06-05 10:17:05 -0300

    atributo de Leche: esPasturizada

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Producto> productos = new ArrayList<>();
-        Leche lecheJersey = new Leche(1500, 1.5);
+        Leche lecheJersey = new Leche(1500, 1.5, true);
         lecheJersey.imprimirInformacion();
     }
 }
--- src/productos/lacteos/Leche.java
+++ src/productos/lacteos/Leche.java
@@ -6,9 +6,12 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Leche extends Producto {
-    public Leche(int precio, double litros) {
+    private boolean esPasteurizada;
+
+    public Leche(int precio, double litros, boolean pasteurizada) {
         this.precioBase = precio;
         this.peso = litros;
+        this.esPasteurizada = pasteurizada;
         this.fechaEnvase = LocalDate.now();
         // TODO Avisar cuando expira.
         this.fechaVencimiento = fechaEnvase.plusDays(7);
@@ -18,8 +21,8 @@ public class Leche extends Producto {
     public void imprimirInformacion() {
         // TODO Hacer table con printf
         DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        System.out.printf("%-36s | %-7s | %-7s | %-10s | %-10s |%n", "ID", "Precio", "Peso", "Envase", "Expiración");
-        System.out.println("=".repeat(84));
-        System.out.printf("%-36s | %-7s | %-7s | %-10s | %-10s |%n", numeroUnico, precioBase + "$", peso + "L", fechaEnvase, fechaVencimiento);
+        System.out.printf("%-36s | %-7s | %-7s | %-10s | %-10s | %-13s |%n", "ID", "Precio", "Peso", "Envase", "Expiración", "Pasteurizada");
+        System.out.println("=".repeat(100));
+        System.out.printf("%-36s | %-7s | %-7s | %-10s | %-10s | %-13s |%n", numeroUnico, precioBase + "$", peso + "L", fechaEnvase, fechaVencimiento, esPasteurizada);
     }
 }
