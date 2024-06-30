e664ca14f1762ce60c65423b979f28e6176d4fba
Tarbolin69 <tarbolin69@gmail.com>
2024-06-30 00:27:09 -0300

    muchos productos agregados

--- src/exhibidores/ExhibidorCLI.java
+++ src/exhibidores/ExhibidorCLI.java
@@ -6,21 +6,23 @@ import productos.Stock;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.StringJoiner;
 
 public class ExhibidorCLI implements Visualizable {
-    static String formatoTabla = "%-3s | %-20s | %-8s | %-7s | %-10s | %-11s |%n";
+    static String formatoTabla = "%-3s | %-25s | %-8s | %-7s | %-10s | %-11s |%n";
+    static String separador = "=".repeat(81);
 
     public void visualizar(Stock stock) {
         List<Producto> productos = stock.getProductos();
         List<String> encabezado = stock.getEncabezado();
         imprimirProductosVencidos(productos);
         System.out.printf(formatoTabla, encabezado.toArray());
-        System.out.println("=".repeat(76));
+        System.out.println(separador);
         for (Producto producto : productos) {
             int index = productos.indexOf(producto);
             imprimirInformacion(producto, index);
         }
-        System.out.println("=".repeat(76));
+        System.out.println(separador);
     }
     public static void imprimirInformacion(Producto producto, int index) {
         DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyy");
@@ -33,10 +35,13 @@ public class ExhibidorCLI implements Visualizable {
     }
     public void imprimirProductosVencidos(List<Producto> productos) {
         LocalDate hoy = LocalDate.now();
+        StringJoiner vencidos = new StringJoiner(", ");
+        System.out.print("Productos vencidos: ");
         for (Producto producto : productos) {
             if (producto.getFechaVencimiento() != null && !hoy.isBefore(producto.getFechaVencimiento())) {
-                System.out.println("* " + productos.indexOf(producto) + " ha expirado.");
+                vencidos.add(String.valueOf(productos.indexOf(producto)));
             }
         }
+        System.out.println(vencidos);
     }
 }
